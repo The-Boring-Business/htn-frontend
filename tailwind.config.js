2b0ccd1bb6f252e973eaf3b786a032fe08092ce3
Risahbh Singh <rishabhsinghs90s@gmail.com>
2021-09-19 02:11:04 +0530

    Add configuration to .gitignore & tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,28 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "landing-page": "url('/landing_page.svg')",
+      },
+    },
+    fontWeight: {
+      medium: 500,
+      "semi-bold": 600,
+      bold: 700,
+    },
+    colors: {
+      gray: "#717579",
+      blue: "#0D34FF",
+      red: "#EB5757",
+      background: "#FBFBFB",
+      white: "#FFFFFF",
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
