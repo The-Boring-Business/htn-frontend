04416d26eaded35718fa12c8f5f594e40ae009d7
Risahbh Singh <rishabhsinghs90s@gmail.com>
2021-09-19 02:12:26 +0530

    Micro UI Fixes in Landing Page

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -12,20 +12,22 @@ const uiConfig = {
 const LandingPage = () => {
   const [user, loading, error] = useAuthState(firebase.auth());
   return (
-    <div className="bg-landing-page bg-contain h-screen w-screen">
+    <div className="bg-landing-page bg-auto h-screen">
       <div className="p-14 pl-20 w-1/3">
-        <img className="h-10 mb-24" src="/logo.svg" alt="logo" />
+        {/* <img className="h-10 mb-14" src="/logo.svg" alt="logo" /> */}
 
-        <h1 className="text-6xl font-bold">
+        <h1 className="text-6xl font-bold mt-10">
           Learn about Fintech with best in class
           <span className="text-blue"> AI Mentor</span>
         </h1>
 
-        <div className="flex flex-row">
+        <div className="flex flex-row mt-10">
           {user ? (
             <div className="flex flex-col">
               <Link href="/dashboard">
-                <a className="hover:bg-blue-500">Go To Dashboard</a>
+                <a className="bg-gray p-4 rounded-lg text-white font-semi-bold hover:bg-blue">
+                  Go To Dashboard
+                </a>
               </Link>
             </div>
           ) : (
