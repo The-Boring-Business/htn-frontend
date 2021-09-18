42b160275881550ae67d8ece19d2de66ec44617e
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-09-18 10:17:44 -0400

    app.js layout done

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import 'tailwindcss/tailwind.css'
+
+import "tailwindcss/tailwind.css";
+import "remixicon/fonts/remixicon.css";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
