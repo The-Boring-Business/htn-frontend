d1bdb399753961981e223d90a7a1bd8f923e4432
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-09-18 14:16:15 -0400

    conditional display when user logs in works

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -10,7 +10,7 @@ const axios = require("axios");
 const dashboard = () => {
   const router = useRouter();
   const [dob, setDob] = useState("");
-  const [authed, setAuthed] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(0);
   const [user, loading, error] = useAuthState(firebase.auth());
 
   useEffect(() => {
@@ -22,6 +22,21 @@ const dashboard = () => {
   }, []);
 
   const [page, setPage] = useState("analytics");
+
+  useEffect(() => {  
+    if (user) {
+      axios
+        .get(
+          "https://balanceed-db.azurewebsites.net/api/check/mail" + user.email
+        )
+        .then((res) => {
+          setLoggedIn(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [user]);
 
   const submitDobForm = (e) => {
     axios
@@ -52,103 +67,101 @@ const dashboard = () => {
     }
   };
 
-  // if (typeof window !== "undefined") {
-  //   const id = localStorage.getItem("id");
-  //   if (!id) {
-  //     return (
-  //       <div>
-  //         <h1>Please Enter Date of Birth</h1>
-  //         <form onSubmit={submitDobForm}>
-  //           <input
-  //             type="date"
-  //             value={dob}
-  //             onChange={(e) => setDob(e.target.value)}
-  //           />
-  //           <button type="submit">Submit</button>
-  //         </form>
-  //       </div>
-  //     );
-  //   }
-  // }
-
-  return (
-    <div className="flex flex-row">
-      <div className="flex flex-col w-1/4">
-        <div
-          onClick={() => setPage("analytics")}
-          className={`flex w-60 py-2.5 px-5 mb-4 text-l ${
-            page == "analytics" && "bg-blue-500 text-white"
-          } rounded-lg cursor-pointer bg-none text-gray font-semi-bold hover:text-white  hover:bg-blue-500`}
-        >
-          <div className="mr-5">
-            <i className="ri-line-chart-line"></i>
-          </div>
-          <h2>Analytics</h2>
-        </div>
-
-        <div
-          onClick={() => setPage("budgeting")}
-          className={`flex w-60 py-2.5 px-5 mb-4 text-l ${
-            page == "budgeting" && "bg-blue-500 text-white"
-          } rounded-lg cursor-pointer bg-none text-gray font-semi-bold hover:text-white hover:bg-blue-500`}
-        >
-          <div className="mr-5">
-            <i class="ri-book-2-line"></i>
-          </div>
-          <h2>Budgeting Basics</h2>
-        </div>
-
-        <div
-          onClick={() => setPage("investment")}
-          className={`flex w-60 py-2.5 px-5 mb-4 text-l ${
-            page == "investment" && "bg-blue-500 text-white"
-          } rounded-lg cursor-pointer bg-none text-gray font-semi-bold hover:text-white  hover:bg-blue-500`}
-        >
-          <div className="mr-5">
-            <i class="ri-book-2-line"></i>
-          </div>
-          <h2>Investment 101</h2>
-        </div>
-
-        <div
-          onClick={() => setPage("income")}
-          className={`flex w-60 py-2.5 px-5 mb-4 text-l ${
-            page == "income" && "bg-blue-500 text-white"
-          } rounded-lg cursor-pointer bg-none text-gray font-semi-bold hover:text-white  hover:bg-blue-500`}
-        >
-          <div className="mr-5">
-            <i class="ri-wallet-line"></i>
-          </div>
-          <h2>Income</h2>
-        </div>
-
-        <div
-          onClick={() => setPage("expenses")}
-          className={`flex w-60 py-2.5 px-5 mb-4 text-l ${
-            page == "expenses" && "bg-blue-500 text-white"
-          } rounded-lg cursor-pointer bg-none text-gray font-semi-bold hover:text-white  hover:bg-blue-500`}
-        >
-          <div className="mr-5">
-            <i class="ri-wallet-line"></i>
-          </div>
-          <h2>Expenses</h2>
-        </div>
-
-        <div
-          onClick={() => setPage("profile")}
-          className={`flex w-60 py-2.5 px-5 mb-4 text-l ${
-            page == "profile" && "bg-blue-500 text-white"
-          } rounded-lg cursor-pointer bg-none text-gray font-semi-bold hover:text-white  hover:bg-blue-500`}
-        >
-          <div className="mr-5">
-            <i class="ri-user-line"></i>
-          </div>
-          <h2>Profile</h2>
-        </div>
+  if (loggedIn == 0) {
+    return (
+      <div>
+        <h1>Please Enter Date of Birth</h1>
+        <form onSubmit={submitDobForm}>
+          <input
+            type="date"
+            value={dob}
+            onChange={(e) => setDob(e.target.value)}
+          />
+          <button type="submit">Submit</button>
+        </form>
       </div>
-      <div className="flex flex-col w-3/4">{renderComponent()}</div>
-    </div>
-  );
+    );
+  } else if (loggedIn == 1) {
+    return (
+      <div className="flex flex-row">
+        <div className="flex flex-col w-1/4">
+          <div
+            onClick={() => setPage("analytics")}
+            className={`flex w-60 py-2.5 px-5 mb-4 text-l ${
+              page == "analytics" && "bg-blue-500 text-white"
+            } rounded-lg cursor-pointer bg-none text-gray font-semi-bold hover:text-white  hover:bg-blue-500`}
+          >
+            <div className="mr-5">
+              <i className="ri-line-chart-line"></i>
+            </div>
+            <h2>Analytics</h2>
+          </div>
+
+          <div
+            onClick={() => setPage("budgeting")}
+            className={`flex w-60 py-2.5 px-5 mb-4 text-l ${
+              page == "budgeting" && "bg-blue-500 text-white"
+            } rounded-lg cursor-pointer bg-none text-gray font-semi-bold hover:text-white hover:bg-blue-500`}
+          >
+            <div className="mr-5">
+              <i class="ri-book-2-line"></i>
+            </div>
+            <h2>Budgeting Basics</h2>
+          </div>
+
+          <div
+            onClick={() => setPage("investment")}
+            className={`flex w-60 py-2.5 px-5 mb-4 text-l ${
+              page == "investment" && "bg-blue-500 text-white"
+            } rounded-lg cursor-pointer bg-none text-gray font-semi-bold hover:text-white  hover:bg-blue-500`}
+          >
+            <div className="mr-5">
+              <i class="ri-book-2-line"></i>
+            </div>
+            <h2>Investment 101</h2>
+          </div>
+
+          <div
+            onClick={() => setPage("income")}
+            className={`flex w-60 py-2.5 px-5 mb-4 text-l ${
+              page == "income" && "bg-blue-500 text-white"
+            } rounded-lg cursor-pointer bg-none text-gray font-semi-bold hover:text-white  hover:bg-blue-500`}
+          >
+            <div className="mr-5">
+              <i class="ri-wallet-line"></i>
+            </div>
+            <h2>Income</h2>
+          </div>
+
+          <div
+            onClick={() => setPage("expenses")}
+            className={`flex w-60 py-2.5 px-5 mb-4 text-l ${
+              page == "expenses" && "bg-blue-500 text-white"
+            } rounded-lg cursor-pointer bg-none text-gray font-semi-bold hover:text-white  hover:bg-blue-500`}
+          >
+            <div className="mr-5">
+              <i class="ri-wallet-line"></i>
+            </div>
+            <h2>Expenses</h2>
+          </div>
+
+          <div
+            onClick={() => setPage("profile")}
+            className={`flex w-60 py-2.5 px-5 mb-4 text-l ${
+              page == "profile" && "bg-blue-500 text-white"
+            } rounded-lg cursor-pointer bg-none text-gray font-semi-bold hover:text-white  hover:bg-blue-500`}
+          >
+            <div className="mr-5">
+              <i class="ri-user-line"></i>
+            </div>
+            <h2>Profile</h2>
+          </div>
+          <h2>{loggedIn}</h2>
+        </div>
+        <div className="flex flex-col w-3/4">{renderComponent()}</div>
+      </div>
+    );
+  }
 };
 
 export default dashboard;
