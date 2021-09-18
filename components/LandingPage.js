d19797cb2d16dbda1c3fd70854ed0385f0a9e167
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-09-18 10:26:44 -0400

    logo and landing page ?!?!?

---
+++ components/LandingPage.js
@@ -0,0 +1,46 @@
+import firebase from "../firebase/clientApp";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Link from "next/link";
+
+const uiConfig = {
+  // signInSuccessUrl: "/dashboard",
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+  signInSuccessUrl: "/dashboard",
+};
+
+const LandingPage = () => {
+  const [user, loading, error] = useAuthState(firebase.auth());
+  return (
+    <div className="bg-landing-page bg-contain h-screen w-screen">
+      <div className="p-14 pl-20 w-1/3">
+        <img className="h-10 mb-24" src="/logo.svg" alt="logo" />
+
+        <h1 className="text-6xl font-bold">
+          Learn about Fintech with best in class
+          <span className="text-blue"> AI Mentor</span>
+        </h1>
+
+        <div className="flex flex-row">
+          {user ? (
+            <div className="flex flex-col">
+              <Link href="/dashboard">
+                <a className="hover:bg-blue-500">Go To Dashboard</a>
+              </Link>
+              <button onClick={() => firebase.auth().signOut()}>
+                Sign Out
+              </button>
+            </div>
+          ) : (
+            <StyledFirebaseAuth
+              uiConfig={uiConfig}
+              firebaseAuth={firebase.auth()}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
