6d51e8fa242c7fc0347954716d3c5bd0e878e228
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-09-18 21:11:44 -0400

    added course card

---
+++ components/CourseCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CourseCard = ({level,name,title}) => {
+    return (
+        <div className="bg-gradient-to-r from-red to-blue p-4">
+            <div className="flex flex-col">
+                <p>{level}</p>
+                <p>{name}</p>
+                <p>{title}</p>
+            </div>
+            
+        </div>
+    )
+}
+
+export default CourseCard
--- components/Profile.js
+++ components/Profile.js
@@ -1,11 +1,14 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import firebase from "../firebase/clientApp";
 import GoalInput from "../components/GoalInput";
+import CourseCard from "./CourseCard";
 import { useRouter } from "next/router";
-
+const axios = require("axios");
 const Profile = () => {
   const router = useRouter();
+  const [myCourses, setMyCourses] = useState([]);
+  const [availableCourses, setAvailableCourses] = useState([]);
   const [user, loading, error] = useAuthState(firebase.auth());
 
   const signOut = () => {
@@ -16,6 +19,39 @@ const Profile = () => {
         router.push("/");
       });
   };
+
+  const getMyCourses = () => {
+    axios
+      .get(
+        "https://balanceed-db.azurewebsites.net/api/" +
+          user.displayName +
+          "/lessons"
+      )
+      .then((res) => {
+        console.log(res.data);
+        setMyCourses(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const getAvailableCourses = () => {
+    axios
+      .get("https://balanceed-db.azurewebsites.net/api/course")
+      .then((res) => {
+        console.log(res.data);
+        setAvailableCourses(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getMyCourses();
+    getAvailableCourses();
+  }, [user]);
 
   return (
     <div className="m-6  py-4 px-6 bg-white rounded-lg drop-shadow-xl">
@@ -37,7 +73,7 @@ const Profile = () => {
               className="bg-red p-4 flex items-center justify-self-end rounded-lg drop-shadow-md text-white font-bold hover:bg-red"
               onClick={signOut}
             >
-             <i className="ri-logout-box-line mr-2"></i> Logout
+              <i className="ri-logout-box-line mr-2"></i> Logout
             </button>
           </div>
         </div>
@@ -45,6 +81,22 @@ const Profile = () => {
           <h1 className="font-bold text-3xl mb-6">Select Your Goal</h1>
           <div className="flex flex-row">
             <GoalInput />
+          </div>
+        </div>
+        <div className="my-2">
+          <h1 className="font-bold text-4xl">Your Courses</h1>
+          <div className="h-32 overflow-x-scroll flex flex-row gap-x-4">
+            {myCourses.map((course) => (
+              <CourseCard title={course.title} name={course.name} level={course.level}/>
+            ))}
+          </div>
+        </div>
+        <div className="my-2">
+          <h1 className="font-bold text-4xl">Available Courses</h1>
+          <div className="h-32 overflow-x-scroll flex flex-row gap-x-4">
+          {availableCourses.map((course) => (
+              <CourseCard title={course.title} name={course.name} level={course.level}/>
+            ))}
           </div>
         </div>
       </div>
